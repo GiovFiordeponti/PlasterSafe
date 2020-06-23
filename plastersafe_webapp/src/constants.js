17ec58d1dcea8e6ede6b8de6539c1b1f1062fac6
FlowerOfTheBridges <gfiordeponti@gmail.com>
2020-06-23 22:47:33 +0200

    github pages

--- plastersafe_webapp/src/constants.js
+++ plastersafe_webapp/src/constants.js
@@ -6,8 +6,8 @@ const COGNITO_APP_WEB_DOMAIN = "plastersafe.auth.us-east-1.amazoncognito.com";
 
 const COGNITO_TOKEN_SCOPE_ARRAY = ['email', 'openid'];
 
-const REDIRECT_URI_SIGN_IN = 'http://localhost:8000';
+const REDIRECT_URI_SIGN_IN = 'https://flowerofthebridges.github.io/PlasterSafe/plastersafe_webapp/';
 
-const REDIRECT_URI_SIGN_OUT = 'http://localhost:8000';
+const REDIRECT_URI_SIGN_OUT = 'https://flowerofthebridges.github.io/PlasterSafe/plastersafe_webapp/';
 
 const COGNITO_USER_POOL_ID = 'us-east-1_sS1Vr0Vhb';
